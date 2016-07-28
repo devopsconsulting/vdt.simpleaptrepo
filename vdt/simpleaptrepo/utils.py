031907a0288d213a665fba3e8cb9f016c1ba914d
maerteijn <martijn@devopsconsulting.nl>
2016-07-29 01:47:32 +0200

    Added a doctest and a manifest.in

--- vdt/simpleaptrepo/utils.py
+++ vdt/simpleaptrepo/utils.py
@@ -3,14 +3,26 @@ import platform
 import subprocess
 import sys
 
-unbuffered_output = os.fdopen(sys.stdout.fileno(), 'w', 0)
-
 
 def repo_root(path):
+    """
+    Returns the root directory from a repo/component
+
+    >>> repo_root("/www/myrepo/test")
+    '/www'
+
+    >>> repo_root("/home/user/myrepo/staging")
+    '/home/user'
+
+    >>> repo_root("/myrepo/staging")
+    '/'
+
+    """
     return os.path.split(os.path.split(path)[0])[0]
 
 
 def write_to_stdout(message):
+    unbuffered_output = os.fdopen(sys.stdout.fileno(), 'w', 0)
     unbuffered_output.write("%s\n" % message)
 
 
