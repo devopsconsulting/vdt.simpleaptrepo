8536f2e64b32c937ac769e96ec0ca39d89978c2d
Martijn Jacobs <martijn@devopsconsulting.nl>
2017-01-09 21:28:07 +0100

    Fixed test for gpg output

--- vdt/simpleaptrepo/tests/test_cli.py
+++ vdt/simpleaptrepo/tests/test_cli.py
@@ -6,18 +6,39 @@ import mock
 
 from vdt.simpleaptrepo import cli
 
+GPG_KEY_OUTPUT = """GPG OUTPUT
+Key created (see above for the hash)
+Now add a repository with the 'create-repo' command
+"""
 
-def mock_check_output(*args, **kwargs):
-    return ""
+GPG_KEY_ERROR = """Usage: create-gpg-key [OPTIONS]
+
+Error: GPG ERROR
+"""
 
 
 class TestCLI(unittest.TestCase):
 
-    @mock.patch('subprocess.check_output', side_effect=mock_check_output)
-    def test_create_gpg_key(self, mock_check_output):
+    @mock.patch('subprocess.check_output', return_value="GPG OUTPUT")
+    def test_create_gpg_key(self, mock_subprocess):
         runner = CliRunner()
         result = runner.invoke(cli.create_key)
+
         # command should be run without error
         self.assertEqual(result.exit_code, 0)
         # and our mock functions should have been called
-        self.assertTrue(mock_check_output.called)
+        self.assertTrue(mock_subprocess.called)
+        # click's output should be captured correctly
+        self.assertEqual(result.output, GPG_KEY_OUTPUT)
+
+    @mock.patch('subprocess.check_output', side_effect=ValueError('GPG ERROR'))
+    def test_create_gpg_key_exception(self, mock_subprocess):
+        runner = CliRunner()
+        result = runner.invoke(cli.create_key)
+
+        # command should raise an error
+        self.assertEqual(result.exit_code, 2)
+        # and our mock functions should have been called
+        self.assertTrue(mock_subprocess.called)
+        # click's output should be captured correctly
+        self.assertEqual(result.output, GPG_KEY_ERROR)
