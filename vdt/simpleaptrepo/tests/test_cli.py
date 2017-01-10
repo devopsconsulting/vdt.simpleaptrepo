8d1692e88a6a90a329d054afdefa144ab1b0de4e
Martijn Jacobs <martijn@devopsconsulting.nl>
2017-01-10 18:01:19 +0100

    More tests

--- vdt/simpleaptrepo/tests/test_cli.py
+++ vdt/simpleaptrepo/tests/test_cli.py
@@ -1,7 +1,6 @@
 import os
 import unittest
 
-
 from click.testing import CliRunner
 
 import mock
@@ -23,6 +22,11 @@ CREATE_REPO_OUTPUT = """Repository 'my_repo' created
 Now add a component with the 'add-component' command
 """
 
+CREATE_REPO_NO_PARAMS_OUTPUT = """Usage: create-repo [OPTIONS] NAME [PATH]
+
+Error: Missing argument "name".
+"""
+
 
 class TestCLI(unittest.TestCase):
 
@@ -32,6 +36,12 @@ class TestCLI(unittest.TestCase):
         cli.apt_repo.path = os.path.join(os.getcwd(), ".simpleapt.ini")
         cli.apt_repo.config.read(cli.apt_repo.path)
         cli.apt_repo.sections = cli.apt_repo.config.sections()
+
+    def tearDown(self):
+        # remove the config file when it exists, and clear the sections
+        if os.path.exists(cli.apt_repo.path):
+            os.remove(cli.apt_repo.path)
+        cli.apt_repo.sections = []
 
     @mock.patch('subprocess.check_output', return_value="GPG OUTPUT")
     def test_create_gpg_key(self, mock_subprocess):
@@ -87,7 +97,37 @@ class TestCLI(unittest.TestCase):
             self.assertTrue(
                 "Add http://<hostname>/my_repo/main / to your sources.list" in result.output)  # noqa
 
-    def tearDown(self):
-        # remove the config file when it exists
-        if os.path.exists(cli.apt_repo.path):
-            os.remove(cli.apt_repo.path)
+            # now we create another repo without a gpg key
+            result = runner.invoke(cli.create_repo, ["another_repo"])
+
+            # a directory with 'my_repo' should be here
+            self.assertTrue(os.path.exists("another_repo"))
+
+            # now create a component
+            result = runner.invoke(
+                cli.add_component, ["another_repo", "main"])
+
+            # command should be run without error
+            self.assertEqual(result.exit_code, 0)
+
+            # click's output should be captured correctly
+            self.assertTrue(
+                "Add http://<hostname>/another_repo/main / to your sources.list" in result.output)  # noqa
+
+    def test_create_repo_no_params(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(cli.create_repo)
+
+            # there is an error as we did not pass any parameter
+            self.assertEqual(result.exit_code, 2)
+            self.assertEqual(result.output, CREATE_REPO_NO_PARAMS_OUTPUT)
+
+    def test_list_repos(self):
+        runner = CliRunner()
+
+        # create a repo and a component
+        runner.invoke(cli.create_repo, ["my_repo", "--gpgkey", "123456"])
+        runner.invoke(cli.add_component, ["my_repo", "main"])
+
+        result = runner.invoke(cli.list_repos)
