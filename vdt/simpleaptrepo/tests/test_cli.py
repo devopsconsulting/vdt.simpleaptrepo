36174269fc92107f800f09042a54bdfc94e1bb87
Martijn Jacobs <martijn@devopsconsulting.nl>
2017-11-18 17:14:16 +0100

    Added / improved some more tests

--- vdt/simpleaptrepo/tests/test_cli.py
+++ vdt/simpleaptrepo/tests/test_cli.py
@@ -33,6 +33,14 @@ LIST_REPOS_OUTPUT = """my_repo (gpgkey: 123456)
    main
 """
 
+UPDATE_REPO_OUTPUT = """
+Creates Packages
+Creates Packages.gz
+Create Release with key 123456
+Create InRelease with key 123456
+Create Releases.gpg with key 123456
+"""
+
 
 class TestCLI(unittest.TestCase):
 
@@ -110,6 +118,13 @@ class TestCLI(unittest.TestCase):
             self.assertTrue(
                 "Add http://<hostname>/my_repo/main / to your sources.list" in result.output)  # noqa
 
+            # adding the same component should raise an error
+            result = runner.invoke(
+                cli.add_component, ["my_repo", "main"])
+
+            self.assertEqual(result.exit_code, 2)
+            self.assertTrue("already exists!" in result.output)
+
             # now we create another repo without a gpg key
             result = runner.invoke(cli.create_repo, ["another_repo"])
 
@@ -127,6 +142,16 @@ class TestCLI(unittest.TestCase):
             self.assertTrue(
                 "Add http://<hostname>/another_repo/main / to your sources.list" in result.output)  # noqa
 
+    def test_create_repo_unkown_path(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(
+                cli.create_repo, ["my_repo", "/unknown/path/"])
+
+            # there is an error as the path is unknown
+            self.assertEqual(result.exit_code, 2)
+            self.assertIn("Path does not exists!", result.output)
+
     def test_create_repo_no_params(self):
         runner = CliRunner()
         with runner.isolated_filesystem():
@@ -134,23 +159,47 @@ class TestCLI(unittest.TestCase):
 
             # there is an error as we did not pass any parameter
             self.assertEqual(result.exit_code, 2)
-            self.assertEqual(result.output, CREATE_REPO_NO_PARAMS_OUTPUT)
+            self.assertIn(CREATE_REPO_NO_PARAMS_OUTPUT, result.output)
 
     def test_list_repos(self):
         runner = CliRunner()
         runner.echo_stdin = True
 
         # create a repo and a component
-        result = runner.invoke(
-            cli.create_repo, ["my_repo", "--gpgkey", "123456"])
-        self.assertEqual(result.exit_code, 0)
-        result = runner.invoke(cli.add_component, ["my_repo", "main"])
-        self.assertEqual(result.exit_code, 0)
+        with runner.isolated_filesystem():
+            result = runner.invoke(
+                cli.create_repo, ["my_repo", "--gpgkey", "123456"])
+            self.assertEqual(result.exit_code, 0)
+            result = runner.invoke(cli.add_component, ["my_repo", "main"])
+            self.assertEqual(result.exit_code, 0)
 
-        # make sure list_repos is showing the correct output
-        result = runner.invoke(cli.list_repos)
-        self.assertEqual(result.exit_code, 0)
-        self.assertEqual(result.output, LIST_REPOS_OUTPUT)
+            # make sure list_repos is showing the correct output
+            result = runner.invoke(cli.list_repos)
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(result.output, LIST_REPOS_OUTPUT)
 
-        # remove the repo
-        shutil.rmtree(os.path.join(os.getcwd(), "my_repo"))
+    @mock.patch('subprocess.check_output', return_value="Mocked output")
+    def test_update_repo(self, mock_subprocess=None):
+        runner = CliRunner()
+
+        with runner.isolated_filesystem():
+            # create a repo and a component
+            result = runner.invoke(
+                cli.create_repo, ["my_repo", "--gpgkey", "123456"])
+            self.assertEqual(result.exit_code, 0)
+            result = runner.invoke(cli.add_component, ["my_repo", "main"])
+            self.assertEqual(result.exit_code, 0)
+
+            # we mock everything, however, the runner still should say that
+            # everything is created / updated
+            result = runner.invoke(
+                cli.update_repo, ["my_repo", "main"])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn(UPDATE_REPO_OUTPUT, result.output)
+
+            # Let's update an component which does not exist
+            result = runner.invoke(
+                cli.update_repo, ["my_repo", "i-am-not-there"])
+            self.assertEqual(result.exit_code, 2)
+            self.assertIn(
+                "Component 'i-am-not-there' does not exist!", result.output)
