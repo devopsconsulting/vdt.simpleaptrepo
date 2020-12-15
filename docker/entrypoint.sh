20975335b12c261ce2f172c382c04d99ec223178
Martijn Jacobs <maerteijn@gmail.com>
2020-12-15 22:36:27 +0100

    Use new GPG key, use python3 here

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
 # runs the unittests
-pip install pip --upgrade
-make test
+pip3 install pip --upgrade
+pip3 install -e .[dev]
+make nosetest
+
 # do some cleanup
 if [ -d "/repos/" ]; then
-      rm -Rf /repos/
+    rm -Rf /repos/
 fi
 
 mkdir /repos
+
 # # now we really create a repo here and add a package
 # # the gpg key is build into the docker image for testing purposes
-simpleapt create-repo myrepo /repos --gpgkey D2E0953C
+simpleapt create-repo myrepo /repos --gpgkey AC46C6AAD5792E7E
 simpleapt add-component myrepo test
 simpleapt list-repos
 cp /usr/local/src/vdt/simpleaptrepo/tests/testdata/*.deb /repos/myrepo/test
